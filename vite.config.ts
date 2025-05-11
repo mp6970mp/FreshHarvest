6ff7fb3450de21cbc7c68cd3f1e77c5554a0cb23
gitea <pp7069pp@gmail.com>
2025-05-10 22:00:27 -0400

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  define: {
+    global: {},
+    'crypto.getRandomValues': 'require("crypto").randomBytes',
+  },
   plugins: [
     react(),
   ],
